b7dff0b43c76ba03a70884fe611baccdeb8e69c3
Éleson Silva <elesonsilva9@gmail.com>
2023-10-24 11:35:40 -0300

    criação do Json das playlist

---
+++ Pages/Principal/Script/conteudo.js
@@ -0,0 +1,34 @@
+let TopPlaylists =[
+    {
+        id:1,
+        img:'Imagens-tela-principal/eletronica-playlist.png',
+        tituloPlayList:'Eletrônica',
+        descPlayList: 'seleção dos Djs'
+    },
+    {
+        id:2,
+        img:'Imagens-tela-principal/festival-playlist.png',
+        tituloPlayList:'Festival',
+        descPlayList: 'As mais tocadas dos festivais'
+    },
+    {
+        id:3,
+        img:'Imagens-tela-principal/playlist-rock.png',
+        tituloPlayList:'Rock',
+        descPlayList: 'tops Rock and Roll'
+    },
+    {
+        id:4,
+        img:'Imagens-tela-principal/playlist-acostic.png',
+        tituloPlayList:'Acustica',
+        descPlayList: 'Mais ouvidas na parada'
+    },
+    {
+        id:5,
+        img:'Imagens-tela-principal/playlist-cover.png',
+        tituloPlayList:'Cover',
+        descPlayList: 'Melhores versões'
+    }
+]
+
+JSON.stringify(TopPlaylists);
