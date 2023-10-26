035faec8c278372250512986d03988ac3a55e2ec
Éleson Silva <elesonsilva9@gmail.com>
2023-10-26 14:56:30 -0300

    Update script.js

--- Pages/Principal/Script/script.js
+++ Pages/Principal/Script/script.js
@@ -9,10 +9,11 @@ TopPlaylists.map((intem, index)=>{
 
     topPLayListItem.querySelector('.swiper-slide img').addEventListener('click', (e)=>{
         e.preventDefault()
-        document.querySelector('.pagina-principal').style.display = 'none'
-        document.querySelector('.lista-aberta').style.display = 'block'
+        transicao()
 
         document.querySelector('.lista-aberta .categoria .img-lista').src = intem.img
+        document.querySelector('.lista-aberta .informacoes .titulo-lista').innerHTML = intem.tituloPlayList
+        document.querySelector('.lista-aberta .informacoes .desc-lista').innerHTML = intem.descPlayList
 
 
     })
@@ -25,6 +26,15 @@ TopAlbuns.map((intem, index)=>{
     albunsItem.querySelector('.swiper-slide img').src = intem.img
     albunsItem.querySelector('.swiper-slide .album-titulo').innerHTML = intem.tituloAlbum
     albunsItem.querySelector('.swiper-slide .desc-album').innerHTML = intem.descAlbum
+
+    albunsItem.querySelector('.swiper-slide img').addEventListener('click', (e)=>{
+        e.preventDefault()
+        transicao()
+
+        document.querySelector('.lista-aberta .categoria .img-lista').src = intem.img
+        document.querySelector('.lista-aberta .informacoes .titulo-lista').innerHTML = intem.tituloAlbum
+        document.querySelector('.lista-aberta .informacoes .desc-lista').innerHTML = intem.descAlbum
+    })
 })
 
 TopMusica.map((intem, index)=>{
@@ -34,6 +44,15 @@ TopMusica.map((intem, index)=>{
     musicaItem.querySelector('.swiper-slide img').src = intem.img
     musicaItem.querySelector('.swiper-slide .musica-titulo').innerHTML = intem.tituloMUsica
     musicaItem.querySelector('.swiper-slide .nome-artista').innerHTML = intem.artista
+
+    musicaItem.querySelector('.swiper-slide img').addEventListener('click', (e)=>{
+        e.preventDefault()
+        transicao()
+
+        document.querySelector('.lista-aberta .categoria .img-lista').src = intem.img
+        document.querySelector('.lista-aberta .informacoes .titulo-lista').innerHTML = intem.tituloMUsica
+        document.querySelector('.lista-aberta .informacoes .desc-lista').innerHTML = intem.artista
+    })
 })
 
 var swiper = new Swiper(".mySwiper", {
@@ -42,4 +61,14 @@ var swiper = new Swiper(".mySwiper", {
     freeMode: false,
     
   });
-   
+
+  
+document.querySelector('.lista-aberta .categoria .informacoes .bi').addEventListener('click',()=>{
+    document.querySelector('.pagina-principal').style.display = 'block'
+    document.querySelector('.lista-aberta').style.display = 'none'
+})  
+
+function transicao(){
+    document.querySelector('.pagina-principal').style.display = 'none'
+    document.querySelector('.lista-aberta').style.display = 'block'
+}
